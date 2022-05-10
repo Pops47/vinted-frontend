91cb61720c7cab2da98e8816a2b0c4d920bb7faa
Pauline Soubrié <pauline_soubrie@hotmail.fr>
2022-05-11 00:49:38 +0200

    css

--- src/containers/Publish.js
+++ src/containers/Publish.js
@@ -19,141 +19,163 @@ function Publish({ token }) {
   return (
     <div>
       {token ? (
-        <form
-          className="publishForm"
-          onSubmit={async (e) => {
-            e.preventDefault();
+        <div className="publish-body">
+          <form
+            className="publish-form"
+            onSubmit={async (e) => {
+              e.preventDefault();
 
-            const formData = new FormData();
-            formData.append("picture", picture);
-            formData.append("title", title);
-            formData.append("description", description);
-            formData.append("brand", brand);
-            formData.append("size", size);
-            formData.append("color", color);
-            formData.append("condition", condition);
-            formData.append("city", city);
-            formData.append("price", price);
-            formData.append("exchanges", exchanges);
+              const formData = new FormData();
+              formData.append("picture", picture);
+              formData.append("title", title);
+              formData.append("description", description);
+              formData.append("brand", brand);
+              formData.append("size", size);
+              formData.append("color", color);
+              formData.append("condition", condition);
+              formData.append("city", city);
+              formData.append("price", price);
+              formData.append("exchanges", exchanges);
 
-            try {
-              const response = await axios.post(
-                "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
-                formData,
-                {
-                  headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "multipart/form-data",
-                  },
-                }
-              );
-              navigate(`/offer/${response.data._id}`);
-            } catch (error) {
-              console.log(error.message);
-            }
-          }}
-        >
-          <h1>Vend ton article</h1>
-          <div>
-            <input
-              type="file"
-              onChange={(e) => setPicture(e.target.files[0])}
-            />
-          </div>
-          <div>
-            <p>Titre</p>
-            <input
-              type="text"
-              name="title"
-              placeholder="ex: Pull gris Nike"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Description</p>
-            <input
-              type="text"
-              name="description"
-              placeholder="ex: très confortable"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Marque</p>
-            <input
-              type="text"
-              name="brand"
-              placeholder="ex: Nike"
-              value={brand}
-              onChange={(e) => setBrand(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Taille</p>
-            <input
-              type="text"
-              name="size"
-              placeholder="ex: XL"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Couleur</p>
-            <input
-              type="text"
-              name="color"
-              placeholder="ex: gris"
-              value={color}
-              onChange={(e) => setColor(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Etat</p>
-            <input
-              type="text"
-              name="condition"
-              placeholder="ex: Bon état"
-              value={condition}
-              onChange={(e) => setCondition(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Ville</p>
-            <input
-              type="text"
-              name="city"
-              placeholder="ex: Paris"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Prix</p>
-            <input
-              type="text"
-              name="price"
-              placeholder="0.00 €"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-          </div>
-          <div className="exchange-line">
-            <input
-              type="checkbox"
-              name="checkbox"
-              value={exchanges}
-              onChange={(e) => setExchanges(e.target.checked)}
-            />{" "}
-            <p>Je suis interressé(e) par les échanges</p>
-          </div>
+              try {
+                const response = await axios.post(
+                  "https://lereacteur-vinted-api.herokuapp.com/offer/publish",
+                  formData,
+                  {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                      "Content-Type": "multipart/form-data",
+                    },
+                  }
+                );
+                navigate(`/offer/${response.data._id}`);
+              } catch (error) {
+                console.log(error.message);
+              }
+            }}
+          >
+            <p>Vends ton article</p>
+            <section className="add-photo">
+              <div className="add-photo-int">
+                <input
+                  className="add-photo-input"
+                  id="add-photo"
+                  type="file"
+                  onChange={(e) => setPicture(e.target.files[0])}
+                />
+                <label for="add-photo" className="add-photo-label">
+                  <span>+ </span> Ajoute une photo
+                </label>
+              </div>
+            </section>
+            <section>
+              <div>
+                <p>Titre</p>
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="ex: Pull gris Nike"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div>
+                <p>Description</p>
+                <input
+                  type="text"
+                  name="description"
+                  placeholder="ex: très confortable"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+            </section>
+            <section>
+              <div>
+                <p>Marque</p>
+                <input
+                  type="text"
+                  name="brand"
+                  placeholder="ex: Nike"
+                  value={brand}
+                  onChange={(e) => setBrand(e.target.value)}
+                />
+              </div>
+              <div>
+                <p>Taille</p>
+                <input
+                  type="text"
+                  name="size"
+                  placeholder="ex: XL"
+                  value={size}
+                  onChange={(e) => setSize(e.target.value)}
+                />
+              </div>
+              <div>
+                <p>Couleur</p>
+                <input
+                  type="text"
+                  name="color"
+                  placeholder="ex: gris"
+                  value={color}
+                  onChange={(e) => setColor(e.target.value)}
+                />
+              </div>
+              <div>
+                <p>Etat</p>
+                <input
+                  type="text"
+                  name="condition"
+                  placeholder="ex: Bon état"
+                  value={condition}
+                  onChange={(e) => setCondition(e.target.value)}
+                />
+              </div>
 
-          <div>
-            <input type="submit" value="Ajouter" />
-          </div>
-        </form>
+              <div>
+                <p>Ville</p>
+                <input
+                  type="text"
+                  name="city"
+                  placeholder="ex: Paris"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                />
+              </div>
+            </section>
+            <section>
+              <div>
+                <p>Prix</p>
+                <input
+                  type="text"
+                  name="price"
+                  placeholder="0.00 €"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="exchange-line">
+                <div>
+                  <input
+                    type="checkbox"
+                    name="checkbox"
+                    value={exchanges}
+                    onChange={(e) => setExchanges(e.target.checked)}
+                  />{" "}
+                  <p>Je suis interressé(e) par les échanges</p>
+                </div>
+              </div>
+            </section>
+
+            <div className="submit-container">
+              <input
+                className="payment-green-button"
+                type="submit"
+                value="Ajouter"
+              />
+            </div>
+          </form>
+        </div>
       ) : (
         <Navigate to="/login" />
       )}
